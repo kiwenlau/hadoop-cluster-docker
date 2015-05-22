adab29c60fefb0981a5ca1dfb446dd7b0bd754b3
kiwenlau <kiwenlau@126.com>
2015-05-22 12:49:41 +0900

    start master container first and use "docker exec" to get into it again

--- start-container.sh
+++ start-container.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# start slave1 container
-sudo docker run -d -t --dns 127.0.0.1 -P --name slave1 -h slave1.kiwenlau.com kiwenlau/hadoop-slave:0.1.0
+# start master container
+sudo docker run -d -t --dns 127.0.0.1 -P --name master -h master.kiwenlau.com -w /root kiwenlau/hadoop-master:0.1.0
 
-# get the IP address of the first container
-FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
+# get the IP address of master container
+FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" master)
+
+# start slave1 container
+sudo docker run -d -t --dns 127.0.0.1 -P --name slave1 -h slave1.kiwenlau.com -e JOIN_IP=$FIRST_IP kiwenlau/hadoop-slave:0.1.0
 
 # start slave2 container
 sudo docker run -d -t --dns 127.0.0.1 -P --name slave2 -h slave2.kiwenlau.com -e JOIN_IP=$FIRST_IP kiwenlau/hadoop-slave:0.1.0
 
-# start master container
-sudo docker run -d -t --dns 127.0.0.1 -P --name master -h master.kiwenlau.com -e JOIN_IP=$FIRST_IP -w /root kiwenlau/hadoop-master:0.1.0
+# create a new Bash session in the master container
+docker exec -it master bash
