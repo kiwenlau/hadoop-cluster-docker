78036b584b2a82c881ede033c7ee38ba1415eab9
cphilo <cphilo@foxmail.com>
2016-05-18 12:07:34 +0800

    remove sudo

--- start-container.sh
+++ start-container.sh
@@ -11,23 +11,23 @@ fi
 	
 
 # delete old master container and start new master container
-sudo docker rm -f master &> /dev/null
+docker rm -f master &> /dev/null
 echo "start master container..."
-sudo docker run -d -t --dns 127.0.0.1 -P --name master -h master.kiwenlau.com -w /root kiwenlau/hadoop-master:0.1.0 &> /dev/null
+docker run -d -t --dns 127.0.0.1 -P --name master -h master.kiwenlau.com -w /root kiwenlau/hadoop-master:0.1.0 &> /dev/null
 
 # get the IP address of master container
-FIRST_IP=$(sudo docker inspect --format="{{.NetworkSettings.IPAddress}}" master)
+FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" master)
 
 # delete old slave containers and start new slave containers
 i=1
 while [ $i -lt $N ]
 do
-	sudo docker rm -f slave$i &> /dev/null
+	docker rm -f slave$i &> /dev/null
 	echo "start slave$i container..."
-	sudo docker run -d -t --dns 127.0.0.1 -P --name slave$i -h slave$i.kiwenlau.com -e JOIN_IP=$FIRST_IP kiwenlau/hadoop-slave:0.1.0 &> /dev/null
+	docker run -d -t --dns 127.0.0.1 -P --name slave$i -h slave$i.kiwenlau.com -e JOIN_IP=$FIRST_IP kiwenlau/hadoop-slave:0.1.0 &> /dev/null
 	i=$(( $i + 1 ))
 done 
 
 
 # create a new Bash session in the master container
-sudo docker exec -it master bash
+docker exec -it master bash
