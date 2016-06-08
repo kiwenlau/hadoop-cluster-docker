c66cb1b95c0bd405705b145dc78956336f06a0f1
kiwenlau <kiwenlau@126.com>
2016-06-09 01:26:34 +0900

    run word count success

--- build-image.sh
+++ build-image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# echo -e "\nbuild docker hadoop-base image\n"
-# sudo docker build -f hadoop-base/Dockerfile -t kiwenlau/hadoop-base:1.0.0 ./hadoop-base
+echo -e "\nbuild docker hadoop-base image\n"
+sudo docker build -f hadoop-base/Dockerfile -t kiwenlau/hadoop-base:1.0.0 ./hadoop-base
 
-# echo ""
+echo ""
 
 
 echo -e "\nbuild docker hadoop-master image\n"
