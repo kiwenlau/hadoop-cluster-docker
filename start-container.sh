aa528f4173be1ddfd457944bc61c4b9120625abf
Wang Xuesong <wxs_tj@163.com>
2016-08-02 15:26:20 +0800

    Update start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -12,6 +12,7 @@ sudo docker run -itd \
                 -p 50070:50070 \
                 -p 8088:8088 \
                 -p 9000:9000 \
+                
                 --name hadoop-master \
                 --hostname hadoop-master \
                 kiwenlau/hadoop:1.0 &> /dev/null
