579229e83d1a5f7e461f50de24a5eb6e1e5cfc5c
Lilia Sfaxi <liliasfaxi@gmail.com>
2018-01-27 11:09:50 +0100

    Initialize container added

--- initialize-container.sh
+++ initialize-container.sh
@@ -5,6 +5,7 @@ N=${1:-3}
 
 
 # start hadoop master container
+sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
                 --net=hadoop \
@@ -19,10 +20,11 @@ sudo docker run -itd \
 i=1
 while [ $i -lt $N ]
 do
+	sudo docker rm -f hadoop-slave$i &> /dev/null
 	echo "start hadoop-slave$i container..."
 	sudo docker run -itd \
-			-p 804$i:8042 \
 	                --net=hadoop \
+			-p 804$i:8042 \
 	                --name hadoop-slave$i \
 	                --hostname hadoop-slave$i \
 	                kiwenlau/hadoop:1.0 &> /dev/null
