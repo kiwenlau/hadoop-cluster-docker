a1e0f1cc906a62799689e9c13588387f5294cf52
Antoine Gicquel <antoine.gicquel@mailoo.org>
2018-02-05 15:21:21 +0100

    add a preserved start mode that doesn't remove previous data

---
+++ start-container-preserved.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# the default node number is 3
+N=${1:-3}
+
+
+# start hadoop master container
+echo "start hadoop-master container..."
+sudo docker run -itd \
+                --net=hadoop \
+                -p 50070:50070 \
+                -p 8088:8088 \
+                --name hadoop-master \
+                --hostname hadoop-master \
+                kiwenlau/hadoop:1.0 &> /dev/null
+
+
+# start hadoop slave container
+i=1
+while [ $i -lt $N ]
+do
+	echo "start hadoop-slave$i container..."
+	sudo docker run -itd \
+	                --net=hadoop \
+	                --name hadoop-slave$i \
+	                --hostname hadoop-slave$i \
+	                kiwenlau/hadoop:1.0 &> /dev/null
+	i=$(( $i + 1 ))
+done 
+
+# get into hadoop master container
+sudo docker exec -it hadoop-master bash
