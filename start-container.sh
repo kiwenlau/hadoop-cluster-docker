4be86e42462429e5d2bc619e20a95cd2f5186712
baoaya <baoaya@gmail.com>
2022-11-12 21:00:03 +0800

    Update start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -13,7 +13,7 @@ sudo docker run -itd \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
-                kiwenlau/hadoop:1.0 &> /dev/null
+                hadoop/centos:1.0 &> /dev/null
 
 
 # start hadoop slave container
@@ -26,7 +26,7 @@ do
 	                --net=hadoop \
 	                --name hadoop-slave$i \
 	                --hostname hadoop-slave$i \
-	                kiwenlau/hadoop:1.0 &> /dev/null
+	                hadoop/centos:1.0 &> /dev/null
 	i=$(( $i + 1 ))
 done 
 
